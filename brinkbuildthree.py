ea024748f5a7adb96c4b0483a332ca14d9b5b629
morsej19 <46002509+morsej19@users.noreply.github.com>
2019-01-08 11:43:55 -0500

    Update brinkbuildthree.py

--- brinkbuildthree.py
+++ brinkbuildthree.py
@@ -12,6 +12,19 @@ def GeraldExtender(neck_size):
     print(" \|/")
     print(" / \\")
 
+def GeraldunExtender(neck_size):
+
+
+    print("  o")
+    for i in range(neck_size):
+        print("  |")
+    print(" \|")
+    print(" / \\")
+
+def GeraldArm(arm_size):
+
+    for i in range(arm_size):
+        print("|")
 
 print("Hello, Gerald has a bizarre disease called Giraffian Neck Syndrome. His neck can grown on command.")
 print("how big should his neck be?")
@@ -20,6 +33,14 @@ user_neck_size = int(input())
 GeraldExtender(user_neck_size)
 #print(user_input)
 
+print("How long should Gerald's arm be?")
+
+user_arm_size = int(input())
+GeraldunExtender(user_neck_size)
+GeraldArm(user_arm_size)
+#print(user_input)
+
+print("Gerald seems to have lost his arm. Oops.")
 print("How many friends should Gerald have?")
 
 gerald_no_friends = int(input())
