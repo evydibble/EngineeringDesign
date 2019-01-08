1faf7f219ed308849366087173aae4426cc5d73a
izzyramras <45952872+izzyramras@users.noreply.github.com>
2019-01-08 11:03:57 -0500

    Update Guessing Game
    changed the range from 1-10 to 1-17, and fixed a spelling error

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,9 @@
 import random
-number = random.randint(1,10)
+number = random.randint(1,17)
 win = False
-print ("I'm thinking of a number between 1 and 10")
+print ("I'm thinking of a number between 1 and 17")
 while not win:
-    guess = int(input("What is your quess?"))
+    guess = int(input("What is your guess?"))
     if guess == number:
         win = True
         print("You're right!")
