34d373781d1459e125f2fc5566a71401ee41bb8c
Max Reisner <32394189+Reisnermax@users.noreply.github.com>
2019-01-08 11:19:46 -0500

    no loops allowed

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,27 @@
+#I remade the guessing game without using any loops
+
 import random
-number = random.randint(1,10)
-win = False
-print ("I'm thinking of a number between 1 and 10")
-while not win:
-    guess = int(input("What is your quess?"))
-    if guess == number:
-        win = True
-        print("You're right!")
+number = random.randint(1,100)
+print("I'm thinking of a number between 1 and 10")
+
+def too_Low():
+    print("Too Low!")
+    try_again()
+    
+def too_High():
+    print("Too High!")
+    try_again()
+
+def win():
+    print("You win! The number was {}". format(number))
+    
+def try_again():
+    guess = int(input("What is your guess?"))
+    if guess > number:
+        too_High()
     elif guess < number:
-        print ("Guess higher")
-    elif guess > number:
-        print ("Guess lower")
-print("You win! The number was {}". format(number))
+        too_Low()
+    elif guess == number:
+        win()
+
+try_again()        
