de0c178297b45970433cb0dce795e693ccfcfd28
izzyramras <45952872+izzyramras@users.noreply.github.com>
2019-01-08 11:36:41 -0500

    Changed the dragon to a beaver
    I changed the animal as well as the way it was printed. Now, the entire beaver image is in a single print command rather than each line being individually printed.

--- maxr.py
+++ maxr.py
@@ -1,19 +1,21 @@
-def make_dragon():
+def make_beaver():
 	print("")
-	print("       \****__              ____          ")                                  
-	print("         |    *****\_      --/ *\-__       ")                                 
-	print("         /_          (_    ./ ,/----'      ")                                 
-	print("           \__         (_./  /             ")                                 
-	print("              \__           \___----^__    ")                                 
-	print("               _/   _                  \   ")                                 
-	print("        |    _/  __/ )\ \ _____         *\ ")                                 
-	print("        |\__/   /    ^ ^       \_________| ")
+	print("""
+	          .="   "=._.---.
+        ."         c ' Y'`p
+       /   ,       `.  w_/
+   jgs |   '-.   /     / 
+ _,..._|      )_-\ \_=.\
+`-....-'`------)))`=-'"`'"
+	""")
+	
 
-number_of_dragons = input("How many dragons? ")
+   
+number_of_beavers = input("How many dragons? ")
 
-if number_of_dragons > 0:
-	for x in range(0, number_of_dragons):
-		make_dragon()
+if number_of_beavers > 0:
+	for x in range(0, number_of_beavers):
+		make_beaver()
 	print("")
 else:
 	print("There must be a valid number")
