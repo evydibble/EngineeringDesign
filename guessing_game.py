29197dacfe516207998532c95f25627824131bf8
Max Reisner <32394189+Reisnermax@users.noreply.github.com>
2019-01-08 20:10:51 -0500

    only recursion

--- guessing_game.py
+++ guessing_game.py
@@ -1,27 +1,18 @@
-#I remade the guessing game without using any loops
+#I remade the guessing game without using any loops and only recursion
 
 import random
 number = random.randint(1,100)
 print("I'm thinking of a number between 1 and 10")
-
-def too_Low():
-    print("Too Low!")
-    try_again()
-    
-def too_High():
-    print("Too High!")
-    try_again()
-
-def win():
-    print("You win! The number was {}". format(number))
     
 def try_again():
     guess = int(input("What is your guess?"))
     if guess > number:
-        too_High()
+        print("Too High")
+        try_again()
     elif guess < number:
-        too_Low()
+        print("Too Low")
+        try_again()
     elif guess == number:
-        win()
+        print("You win! The number was {}". format(number))
 
 try_again()        
