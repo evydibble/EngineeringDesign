9b3191c75e0ad9f1530b413e33c69cb550f15b9d
3huangl19 <46002564+3huangl19@users.noreply.github.com>
2019-01-09 20:43:03 -0500

    Update LinaQuote.py

--- LinaQuote.py
+++ LinaQuote.py
@@ -2,3 +2,6 @@ GGGGGGGGGGGGGGGGGG = 'GGGGGGGGGGGGGGGGGG'
 
 for letter in GGGGGGGGGGGGGGGGGG:
    print("Good things come to those who wait")
+
+for GGGGGGGGGGGGGGGGGG in range(0, 15):
+    print "Things worth having don't come easy %d" % (GGGGGGGGGGGGGGGGGG)
