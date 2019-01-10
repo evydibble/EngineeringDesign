e0df7c6cef64729057674674bef1f1728d9aeee3
abbyspencer <46002639+abbyspencer@users.noreply.github.com>
2019-01-10 08:30:24 -0500

    Update aws.py

--- aws.py
+++ aws.py
@@ -1,1 +1,15 @@
-print('hello world')
+import random
+n = random.randint(0, 100)
+guess = int(raw_input("Chose a number from 0-100: "))
+while n != "guess":
+    print
+    if guess < n:
+        print "try a higher number"
+        guess = int(raw_input("Chose a number from 0-100: "))
+    elif guess > n:
+        print "try a lower number"
+        guess = int(raw_input("Chose a number from 0-100: "))
+    else:
+        print "that's correct!"
+        break
+    print
